238d44950efe34d60b9dc19db8fb7d69f5bef7d1
James Kieltyka <james.kieltyka@digio.com.au>
2018-08-04 22:56:04 +1000

    Small update to dynamo functions and begun s3 pull function

--- cmd/main.go
+++ cmd/main.go
@@ -7,10 +7,12 @@ import (
 )
 
 func Handler(request events.CloudWatchEvent) {
-	scheduledGet.WithDynamoDBGetLatest("https://asic.gov.au/Reports/Daily/2018/07/RR20180726-001-SSDailyAggShortPos.csv", "test")
+	clients := scheduledGet.GenerateAWSClients("dynamoDB", "s3")
+	scheduledGet.WithDynamoDBGetLatest("https://asic.gov.au/Reports/Daily/2018/07/RR20180726-001-SSDailyAggShortPos.csv", "test", clients)
 }
 
 func main() {
-	scheduledGet.GenerateAWSClients("dynamoDB")
+	//clients := scheduledGet.GenerateAWSClients("dynamoDB", "s3")
+	// scheduledGet.WithDynamoDBGetLatest("https://asic.gov.au/Reports/Daily/2018/07/RR20180726-001-SSDailyAggShortPos.csv", "test", clients)
 	lambda.Start(Handler)
 }
--- pkg/scheduledServices/scheduledGet.go
+++ pkg/scheduledServices/scheduledGet.go
@@ -1,14 +1,37 @@
 package scheduledGet
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 
 	"fmt"
 	"net/http"
 )
 
-var dynamoclient *dynamodb.DynamoDB
+type clientsStruct struct {
+	dynamoclient     *dynamodb.DynamoDB
+	s3DownloadClient *s3manager.Downloader
+	s3UploadClient   *s3manager.Uploader
+}
+
+// GenerateAWSClients generates new AWS clients based on string array
+func GenerateAWSClients(clients ...string) *clientsStruct {
+	sess := session.Must(session.NewSession())
+	clientStruct := new(clientsStruct)
+	for _, client := range clients {
+		switch client {
+		case "s3":
+			clientStruct.s3DownloadClient = s3manager.NewDownloader(sess)
+			clientStruct.s3UploadClient = s3manager.NewUploader(sess)
+		case "dynamoDB":
+			clientStruct.dynamoclient = dynamodb.New(sess)
+		}
+	}
+	return clientStruct
+}
 
 // WithDynamoDBGetLatest
 // Checks the last updated time of the url, checks dynamoDB for last recorded record update
@@ -16,7 +39,7 @@ var dynamoclient *dynamodb.DynamoDB
 // inputs:
 //	url: url for the request
 //	item: dynamoDB item to be updated
-func WithDynamoDBGetLatest(url string, item string) {
+func WithDynamoDBGetLatest(url string, item string, client *clientsStruct) {
 	resp, err := http.Head(url)
 
 	if err != nil {
@@ -26,42 +49,52 @@ func WithDynamoDBGetLatest(url string, item string) {
 	lastModified := resp.Header.Get("Last-Modified")
 
 	fmt.Println(lastModified)
-	FetchDynamoDBLastModified("lastUpdate", item)
-	PutDynamoDBLastModified("lastUpdate", item, "testTime2")
-}
-
-// GenerateAWSClients generates new AWS clients based on string array
-func GenerateAWSClients(clients ...string) {
-	sess := session.Must(session.NewSession())
-	for _, client := range clients {
-		switch client {
-		case "s3":
-			fmt.Println("TODO")
-		case "dynamoDB":
-			dynamoclient = dynamodb.New(sess)
-		}
-	}
+	FetchDynamoDBLastModified("lastUpdate", item, client)
+	PutDynamoDBLastModified("lastUpdate", item, "testTime2", client)
+	FetchMapFileFromS3("shortedappjmk", "testCsv.csv", client, "")
 }
 
 // FetchDynamoDBLastModified pulls latest field update date
-func FetchDynamoDBLastModified(tableName string, keyName string) (string, error) {
-	resp, err := dynamoclient.GetItem(&dynamodb.GetItemInput{
+func FetchDynamoDBLastModified(tableName string, keyName string, client *clientsStruct) (string, error) {
+	resp, err := client.dynamoclient.GetItem(&dynamodb.GetItemInput{
 		Key:       map[string]*dynamodb.AttributeValue{"name_id": &dynamodb.AttributeValue{S: &keyName}},
 		TableName: &tableName,
 	})
+
 	if err != nil {
 		return "", err
 	}
-	return resp.Item[keyName].String(), nil
+
+	return *resp.Item["date"].S, nil
 }
 
 // PutDynamoDBLastModified updates latest field update date
-func PutDynamoDBLastModified(tableName string, keyName string, time string) error {
-	_, err := dynamoclient.PutItem(&dynamodb.PutItemInput{
+func PutDynamoDBLastModified(tableName string, keyName string, time string, client *clientsStruct) error {
+	_, err := client.dynamoclient.PutItem(&dynamodb.PutItemInput{
 		Item: map[string]*dynamodb.AttributeValue{"name_id": &dynamodb.AttributeValue{S: &keyName},
 			"date": &dynamodb.AttributeValue{S: &time}},
 		TableName: &tableName,
 	})
 
 	return err
+}
+
+// FetchMapFileFromS3 loads a csv mapping file to generate a key value reference
+// inputs:
+//	- bucketName: the name of the bucket the file is being retrieved from
+//	- key: the key for the s3 object
+//	- valueStruct: an example of the file structrue to use
+//map[string]interface{}
+func FetchMapFileFromS3(bucketName string, key string, client *clientsStruct, valueStruct interface{}) {
+
+	//create a buffer to write content
+	buf := aws.NewWriteAtBuffer([]byte{})
+	//Get the file from s3
+	n, err := client.s3DownloadClient.Download(buf, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(key),
+	})
+	fmt.Println(n, err)
+	fmt.Println(buf)
+
 }
