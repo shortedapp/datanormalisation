f3dee4aec93d9da5f5d57d9ce63a138415a16807
jkieltyka <james.kieltyka@gmail.com>
2018-09-11 22:18:00 +1000

    added some notes on athena requirements

--- internal/handlerhelper/topmoversingest/helper.go
+++ internal/handlerhelper/topmoversingest/helper.go
@@ -9,18 +9,27 @@ type Topmoversingestor struct {
 func (t *Topmoversingestor) IngestMovement(tableName string) {
 	//run Athena Query
 	// "CREATE EXTERNAL TABLE IF NOT EXISTS test.testShorts (
-	// 	code string,
-	// 	name string,
-	// 	shorts int,
-	// 	total int,
-	// 	percent float,
-	// 	industry string
-	//   )
-	//   ROW FORMAT DELIMITED
-	//   FIELDS TERMINATED BY ','
-	//   ESCAPED BY '\\'
-	//   LINES TERMINATED BY '\n'
-	//   LOCATION 's3://shortedappjmk/testShortedData/'
-	//   TBLPROPERTIES ('has_encrypted_data'='false')"
+	// 		result array<struct<code:string,
+	// 		name:string,
+	// 		shorts:BIGINT,
+	// 		total:BIGINT,
+	// 		percent:float,
+	// 		industry:string>>
+	// )
+	// ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe' LOCATION 's3://shortedappjmk/testShortedData/' TBLPROPERTIES ('has_encrypted_data'='false');"
+
+	// EXAMPLE QUERY
+	// WITH stocks AS
+	//     (SELECT "$path" AS dateTime,
+	//          stock.code AS code,
+	//          stock.percent AS percent
+	//     FROM "test"."testshorts", unnest(result) t(stock) )
+	// SELECT *
+	// FROM stocks
+	// ORDER BY stocks.percent DESC limit 10;
+
+	//THE ABOVE QUERY NEEDS REGEX FOR THE FILE NAME
+
+	//TODO MODIFY COMBINED SHORTS TO HAVE A KEY FOR ATHENA
 
 }
